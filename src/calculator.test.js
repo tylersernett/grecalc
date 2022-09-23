4a53bd26766da9aa2603f20bff94979463144d96
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-09-23 16:38:15 -0400

    Add more tests - plusmins & memory recall

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -81,6 +81,34 @@ describe("TestCalc", () => {
         userEvent.click(memrecall);
         expect(display).toHaveTextContent(`27`);
         userEvent.click(clear);
+        userEvent.click(four);
+        userEvent.click(times);
+        userEvent.click(memrecall);
+        userEvent.click(equals);
+        expect(display).toHaveTextContent(`108`);
+
+        userEvent.click(memclear);
+        
+        //clear entry
+        userEvent.click(clear);
+        userEvent.click(one);
+        userEvent.click(zero);
+        userEvent.click(zero);
+        userEvent.click(plus);
+        userEvent.click(five);
+        userEvent.click(five);
+        userEvent.click(clearEntry);
+        userEvent.click(eight);
+        userEvent.click(eight);
+        userEvent.click(equals);
+        expect(display).toHaveTextContent(`188`);
+
+        //positive root
+        userEvent.click(clear);
+        userEvent.click(nine);
+        userEvent.click(root);
+        expect(display).toHaveTextContent(`3.`);
+        userEvent.click(clear);
 
         //negative root
         userEvent.click(clear);
@@ -89,6 +117,52 @@ describe("TestCalc", () => {
         userEvent.click(root);
         expect(display).toHaveTextContent(`ERROR`);
         userEvent.click(clear);
+    });
+
+    it("plusminus check", () => {
+        render(<TestCalc />);
+        const plusminus = screen.getByText(/±/i);
+        const times = screen.getByText(/×/i);
+        const minus = screen.getByText(/–/i);
+
+        userEvent.click(clear);
+        userEvent.click(five);
+        userEvent.click(plusminus);
+        expect(display).toHaveTextContent(/-5./);
+        userEvent.click(times);
+        userEvent.click(five);
+        userEvent.click(equals);
+        expect(display).toHaveTextContent(/-25./);
+        userEvent.click(plusminus);
+        expect(display).toHaveTextContent(/25./);
+        userEvent.click(minus);
+        userEvent.click(five);
+        userEvent.click(plusminus);
+        userEvent.click(equals);
+        expect(display).toHaveTextContent(/30./);
+
+        userEvent.click(clear);
+    });
+
+    it("plusminus memory check", () => {
+        render(<TestCalc />);
+        const plusminus = screen.getByText(/±/i);
+        const times = screen.getByText(/×/i);
+        const minus = screen.getByText(/–/i);
+        const memrecall = screen.getByText(/MR/i);
+        const memclear = screen.getByText(/MC/i);
+        const memadd = screen.getByText(/M\+/i);
+
+        userEvent.click(clear);
+        userEvent.click(nine);
+        userEvent.click(memadd);
+        userEvent.click(clear);
+        userEvent.keyboard("18*");
+        userEvent.click(memrecall);
+        userEvent.click(plusminus);
+        userEvent.click(equals);
+        expect(display).toHaveTextContent(/-162./);
+
     });
 
     it("PEMDAS check", () => {
@@ -151,6 +225,18 @@ describe("TestCalc", () => {
         expect(display).toHaveTextContent(/^ERROR$/);
     });
 
+    it("8 digit display cap: ensure decimals are lossy", () => {
+        render(<TestCalc />);
+        userEvent.keyboard("1/7=*7=");
+        expect(display).toHaveTextContent(/^0.9999997$/);
+    });
+
+    it("8 digit display cap: lose decimal when non-decimal figure added", () => {
+        render(<TestCalc />);
+        userEvent.keyboard("9.0000001+3=");
+        expect(display).toHaveTextContent(/^12.$/);
+    });
+
     it("accept 0 as initial entry", () => {
         render(<TestCalc />);
         userEvent.keyboard("0-5=");
@@ -163,83 +249,10 @@ describe("TestCalc", () => {
         expect(display).toHaveTextContent("ERROR");
     });
 
-    
+    it("only use most recent operand", () => {
+        render(<TestCalc />);
+        userEvent.keyboard("7/-*+3=")
+        expect(display).toHaveTextContent("10");
+    });
 
 });
-
-// beforeAll(() => {
-//     render(<Calculator />);
-//     const display = screen.getByText(/0./i)
-//     const one = screen.getByText(/1/i);
-//     const two = screen.getByText(/2/i);
-//     const three = screen.getByText(/3/i);
-//     const four = screen.getByText(/4/i);
-//     const five = screen.getByText(/5/i);
-//     const six = screen.getByText(/6/i);
-//     const seven = screen.getByText(/7/i);
-//     const eight = screen.getByText(/8/i);
-//     const nine = screen.getByText(/9/i);
-//     const zero = screen.getByText(/^0$/i);
-//     const point = screen.getByText(/^\.$/);
-
-//     const memrecall = screen.getByText(/MR/i);
-//     const memclear = screen.getByText(/MC/i);
-//     const memadd = screen.getByText(/M\+/i);
-//     const parenleft = screen.getByText(/\(/);
-//     const parenright = screen.getByText(/\)/);
-
-//     const plus = screen.getByText(/^\+$/i);
-//     const minus = screen.getByText(/–/i);
-//     const times = screen.getByText(/×/i);
-//     const divide = screen.getByText(/÷/i);
-//     const root = screen.getByText(/√/i);
-//     const plusminus = screen.getByText(/±/i);
-//     const equals = screen.getByText(/=/i);
-
-//     const clear = screen.getByText(/^C$/i);
-//     const clearEntry = screen.getByText(/^CE$/i);
-// })
-
-/*
-test('PEMDAS', () => {
-    
-    // setup();
-
-    
-
-    userEvent.click(clear);
-    userEvent.click(one);
-    userEvent.click(divide);
-    userEvent.click(zero);
-    userEvent.click(equals);
-    expect(display).toHaveTextContent(`ERROR`);
-    userEvent.click(clear);
-
-
-});
-
-test('PEM', () => {
-    // setup();
-    userEvent.click(clear);
-    userEvent.click(one);
-    userEvent.click(plus);
-    userEvent.click(two);
-    userEvent.click(times);
-    userEvent.click(three);
-    userEvent.click(equals);
-    expect(display).toHaveTextContent(`7`);
-    userEvent.click(clear);
-})
-
-test('negative root', () => {
-    // setup();
-    userEvent.click(clear);
-    userEvent.click(five);
-    userEvent.click(plusminus);
-    userEvent.click(root);
-    expect(display).toHaveTextContent(`ERROR`);
-    userEvent.click(clear);
-
-});
-
-*/
