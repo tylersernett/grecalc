4979ff5d3b2331baa969ddee5c5942fa1e301330
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-05 16:17:04 -0400

    Improve SVG formatting and top banner borders

--- src/timer.js
+++ src/timer.js
@@ -106,18 +106,18 @@ function Timer({ timerInputIsOpen, setTimerInputIsOpen }) {
 
     const HideTimeDisplay = () => {
         return (
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="-1 -1 18 18">
+                <path stroke="currentColor" stroke-width="1" d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path stroke="currentColor" stroke-width="1" d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
               </svg> Hide Time</span>
         );
     };
 
     const ShowTimeDisplay = () => {
         return (
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="-1 -1 18 18">
+                <path stroke="currentColor" stroke-width="1" d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                <path stroke="currentColor" stroke-width="1" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
               </svg> Show Time</span>
         );
     };
@@ -153,6 +153,7 @@ function Timer({ timerInputIsOpen, setTimerInputIsOpen }) {
                     </button>
                 </div>
             </div>
+
             <div className='timer-banner'>
                 <span className='timer-display'>{hide? "" : seconds < 0 ? "Time expired" : display.hours + ":" + display.minutes + ":" + display.seconds}</span>
                 <span className='hide-display' onClick={toggleHide}>{hide ? ShowTimeDisplay() : HideTimeDisplay()}</span>
