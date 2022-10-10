d63508c7b42879bf5486644d742589e47005bc7e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-10 00:33:11 -0400

    Restrict drag and drop to top of calculator only

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import reportWebVitals from './reportWebVitals';
 
 const par = {
   // position: 'absolute',
-  backgroundColor: 'red',
+  // backgroundColor: 'red',
   //height: '500px',
   minHeight: 'calc(100vh)',
   width: '100%',
@@ -29,49 +29,53 @@ const DraggableItem = {
   // left: '20px',
   // top: '20px',
   width: '300px',
+  height: '50px',
   // height: '100px',
-  backgroundColor: 'green'
+  // backgroundColor: 'green'
 };
 
 function App() {
-  //declare here, becuase both Timer and Calculator use it
+  //declare timerInputIsOpen here, becuase both Timer and Calculator use it
   const [timerInputIsOpen, setTimerInputIsOpen] = useState(false);
 
   //drag code mod'd from //from https://stackoverflow.com/questions/20926551/recommended-way-of-making-react-component-div-draggable
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const elementRef = useRef(null);
 
-  const onMouseDown = useCallback(
+  const dragAndDrop = useCallback(
     (event) => {
-      const onMouseMove = (MouseEvent) => {
-        const element = elementRef.current;
-        let maxWidth = element.parentElement.clientWidth;
-        let maxHeight = element.parentElement.clientHeight;
-        let xSum = position.x + MouseEvent.movementX;
-        let ySum = position.y + MouseEvent.movementY;
-        let calcWidth = 256;
-        let calcHeight = 300;
-        //TODO: document.getElementById('masonryParent').children[0].style.height
-        let bannerHeight = 130;
-        if (xSum > 0 && xSum + calcWidth < maxWidth) {
-          position.x = xSum;
-        }
-        if (ySum > 0 && ySum + calcHeight + bannerHeight < maxHeight) {
-          position.y = ySum;
-        }
-        console.log(position.x, position.y, maxHeight);
+      if (event.target.className === "calc-top") { //only allow dragging on uppermost part of calculator
+        const onMouseMove = (MouseEvent) => {
+          const element = elementRef.current;
+          let maxWidth = element.parentElement.clientWidth;
+          let maxHeight = element.parentElement.clientHeight;
+          let xSum = position.x + MouseEvent.movementX;
+          let ySum = position.y + MouseEvent.movementY;
+          let calcWidth = 256 + 10;
+          let calcHeight = 300;
+          //TODO: document.getElementById('masonryParent').children[0].style.height
+          let bannerHeight = 130;
+          if (xSum > 0 && xSum + calcWidth < maxWidth) {
+            position.x = xSum;
+          }
+          if (ySum > 0 && ySum + calcHeight + bannerHeight < maxHeight) {
+            position.y = ySum;
+          }
+          console.log(position.x, position.y, maxHeight);
 
-        if (element) {
-          element.style.transform = `translate(${position.x}px, ${position.y}px)`;
-        }
-        setPosition(position);
-      };
-      const onMouseUp = () => {
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-      };
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
+          if (element) {
+            element.style.transform = `translate(${position.x}px, ${position.y}px)`;
+          }
+          setPosition(position);
+        };
+
+        const onMouseUp = () => {
+          document.removeEventListener("mousemove", onMouseMove);
+          document.removeEventListener("mouseup", onMouseUp);
+        };
+        document.addEventListener("mousemove", onMouseMove);
+        document.addEventListener("mouseup", onMouseUp);
+      }
     },
     [position, setPosition, elementRef]
   );
@@ -80,7 +84,7 @@ function App() {
     <>
       <Timer timerInputIsOpen={timerInputIsOpen} setTimerInputIsOpen={setTimerInputIsOpen} />
       <div style={par}>
-        <div ref={elementRef} style={DraggableItem} onMouseDown={onMouseDown}>
+        <div ref={elementRef} style={DraggableItem} onMouseDown={dragAndDrop}>
           <Calculator timerInputIsOpen={timerInputIsOpen} />
         </div>
       </div>
