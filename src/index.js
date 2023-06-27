1f67cfc4057c147461797ce00ccfee220f67f34f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-27 18:24:40 -0400

    Delete old comments

--- src/index.js
+++ src/index.js
@@ -83,29 +83,6 @@ function App() {
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(<App />);
 
-// module.exports = Calculator;
-
-//redo equalsClickHandle switch statement for less redundancy
-//how does GRE calc handle -? as negative, or always subtract?
-//add memory functions
-//sqrt: 9+, 9/, etc
-//memory recall, then NUMBERBUTTON ==> append to string
-//after recall -- reset string UNLESS its operand! +-*/
-//should it just be... result = recall, string = ""???
-//paren + memRecall + operand ==> ( paren removed??
-//14 / memRecall ... not recalling?
-//when closing Parens... something needs update
-//5 + memclear + 9 should not be 59
-//store 5
-//recall 5
-//clear 5, 9 ==> 59
-//BUG: result or num holding UNROUNDED values for lonnnnng decimals
-//FIXED!
-//TEST: 1/7 * 7 = 0.9999997
-//CHECK: behavior when ERROR is in display. must you hit C to move on?
-//CSS: everything gets darker on click, and gets BOX highlight on hover
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
